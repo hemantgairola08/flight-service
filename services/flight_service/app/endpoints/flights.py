fe8ffecf6cc67ed06ee1a4891c040796b9537ff3
Hemant Gairola <hemant.gairola@delphix.com>
2024-04-25 00:07:58 +0530

    Flight service and Transaction service

--- services/flight_service/app/endpoints/flights.py
+++ services/flight_service/app/endpoints/flights.py
@@ -24,15 +24,10 @@ def search_flights(payload: dict):
     rows = cursor.fetchall()
     database.close_connection(conn)
     print(rows)
-    filtered_flights = [Flight(id=row[0], origin=row[1], destination=row[2], departure_time=row[3], capacity=row[4], available_seats=row[5]) for row
+    filtered_flights = [Flight(id=row[0], origin=row[1], destination=row[2], departure_time=row[3], capacity=row[4], available_seats=row[5], price=row[6]) for row
             in rows]
 
     if not filtered_flights:
         raise HTTPException(status_code=404, detail="No flights found")
 
     return filtered_flights
-
-# if __name__ == "__main__":
-#     import uvicorn
-#
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
